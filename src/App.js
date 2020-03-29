8536f03bdc5962d521c191d455765c9fc4552be3
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2020-03-29 20:52:12 +0900

    added several paths and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import EnglishHome from "./english/EnglishHome";
+import EnglishProjects from "./english/EnglishProjects";
+import JapaneseHome from "./english/EnglishHome";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    localStorage.setItem("apiUrl", 'https://mutsuki-api.herokuapp.com');
+    localStorage.setItem("clientUrl", 'http://www.mutsukiuchiyama.com');
+    if(process.env.NODE_ENV !== 'production') {
+      localStorage.setItem("apiUrl", 'http://localhost:3001');
+      localStorage.setItem("clientUrl", 'http://localhost:3000');
+    }
+    console.log(localStorage.clientUrl)
+  }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" component={EnglishHome} />
+          <Route path="/en" exact component={EnglishHome} />
+          <Route path="/jp" exact component={JapaneseHome} />
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/japanese/JapaneseHome.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+class Home extends Component {
+  constructor(props){
+    super();
+    const currentURL = localStorage.clientUrl;
+
+    this.state = {
+      rangeValue: 0,
+      specialImageSource: [`${currentURL}/images/special_effect/afrika.JPG`, `${currentURL}/images/special_effect/IT.png`],
+      beautyImageSource: [`${currentURL}/images/beauty/beauty1.JPG`, `${currentURL}/images/beauty/marylyn.png`],
+      currentImage: 0
+    }
+  }
+
+  switchImage = () => {
+    if (this.state.currentImage < this.state.beautyImageSource.length -1 ){
+      this.setState({
+        currentImage: this.state.currentImage + 1
+      })
+    } else {
+        this.setState({
+          currentImage: 0
+        });
+      }
+      return this.currentImage;
+    }
+
+  componentDidMount(){
+    setInterval(this.switchImage, 5000);
+
+  }
+
+  changeValue = (event) => {
+      this.setState({
+        rangeValue: event.target.value
+      })
+      console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="container">
+      <p className = "upper-left-bracket">「</p>
+      <p className = "lower-left-bracket">「</p>
+      <p className = "upper-right-bracket">「</p>
+      <p className = "lower-right-bracket">「</p>
+<div className="row">
+        <div id="left-side">
+          <div className = {this.state.rangeValue <= 50 ? "special-effect-name" : "beauty-name" } id="name">
+            <h1>MUTSUKI</h1>
+            <h1>UCHIYAMA</h1>
+          </div>
+          <div id="slider">
+            <input type="range" className="custom-range" id="customRange1" onChange={this.changeValue} value={this.state.rangeValue}></input>
+          </div>
+          <div className = {this.state.rangeValue <= 50 ? "special-effect-arrow" : "beauty-arrow" } id="arrow-down">
+          <i className="fas fa-chevron-down"></i>
+          <i className="fas fa-chevron-down"></i>
+          </div>
+          <div className="row">
+          <h2 className="active-language">EN</h2>
+          <h2>JP</h2>
+          </div>
+        </div>
+        <div id="right-side">
+        <button className = "btn btn-primary">hello world</button>
+          <img className="image" src={this.state.rangeValue <= 50 ? this.state.specialImageSource[this.state.currentImage] : this.state.beautyImageSource[this.state.currentImage] } alt="image" id="special-effect-image"/>
+          {/* navbar */}
+        </div>
+      </div>        
+      </div>
+    );
+  }
+}
+
+export default Home;
